5a701284f8cdf205847f795c757ddfb5d59bc456
unknown <kristinabog7@hotmail.com>
2023-02-01 23:21:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?lat=37.98&lon=23.72&appid=bbcee01db995d3aea21f15b3fa745616';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
