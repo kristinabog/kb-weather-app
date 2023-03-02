c337dec3ea054a4969016b1c963d850805e7ff39
unknown <kristinabog7@hotmail.com>
2023-03-02 21:43:22 +0200

    Added structure

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import './App.css';
 
 function App() {
 
-  // const url = 'https://api.openweathermap.org/data/2.5/weather?lat=37.98&lon=23.72&appid=bbcee01db995d3aea21f15b3fa745616';
+  // const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=bbcee01db995d3aea21f15b3fa745616';
 
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Rome</p>
+          </div>
+          <div className="temp">
+            <h1>20°C</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>25°C</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>10 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
